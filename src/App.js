23ea072f1baadd8951f0e3c8679a602247ca001b
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-07 16:32:41 +0530

    try to add map chart
    try to add india chart in map chart

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import React,{Component} from 'react';
 import FusionCharts from 'fusioncharts';
 import Maps from 'fusioncharts/fusioncharts.maps';
 import World from 'fusioncharts/maps/fusioncharts.world';
+import India from 'fusioncharts/maps/india';
 import ReactFC from 'react-fusioncharts';
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
 // import bootstrap css
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactFC.fcRoot(FusionCharts, Maps, World, FusionTheme);
+ReactFC.fcRoot(FusionCharts, Maps, India, FusionTheme);
 
-// column 3d chart configuration settings
+// map chart configuration settings
 const chartConfigs_map = {
   type: 'world',
   width: '100%',
@@ -82,6 +83,77 @@ const chartConfigs_map = {
   },
 };
 
+// asia map chart configuration settings
+const chartConfigs_asiaMap = {
+  type: 'maps/india',
+  width: '100%',
+  height: 450,
+  dataFormat: 'json',
+  dataSource: {
+    "chart": {
+      "theme": "fusion",
+      "caption": "Increased Population History by country Last Year",
+      "xAxisName": "Population",
+      "yAxisName": "Growth",
+      "entityFillHoverColor": "#FFF9C4",
+      "numberSuffix": "%",
+      "showBorder": "1",
+      "borderColor": "#ccc",
+      "bgColor": "#FFFFFF",
+      //Tooltip customization
+      "toolTipBorderColor": "#666666",
+      "toolTipBgColor": "#efefef",
+      "toolTipBgAlpha": "80",
+      "showToolTipShadow": "1",
+      //Theme
+      "plotBorderThickness": "0.25",
+      "showxaxispercentvalues": "1",
+      "showValues": "1"
+    },
+    "colorrange": {
+      "color": [{
+        "minvalue": "0",
+        "maxvalue": "100",
+        "color": "#FFD74D"
+      }, {
+        "minvalue": "0",
+        "maxvalue": "100",
+        "color": "#FB8C00"
+      }, {
+        "minvalue": "0",
+        "maxvalue": "100",
+        "color": "#E65100"
+      }]
+    },
+    "data": [{
+      "id": "NA",
+      "value": "20",
+      "showLabel": "1"
+    }, {
+      "id": "SA",
+      "value": "30",
+      "showLabel": "1"
+    }, {
+      "id": "AS",
+      "value": "48",
+      "showLabel": "1"
+    }, {
+      "id": "EU",
+      "value": "20",
+      "showLabel": "1"
+    }, {
+      "id": "AF",
+      "value": "2.58",
+      "showLabel": "1"
+    }, {
+      "id": "AU",
+      "value": "15",
+      "showLabel": "1"
+    }]
+  },
+};
+
+
 class App extends Component {
   render() {
     return (
@@ -89,9 +161,15 @@ class App extends Component {
           <div className="row" style={{marginTop:50}}>
 
             {/*Map section*/}
-            <div className="col-sm-12">
+            {/* <div className="col-sm-12">
               <h3 className="text-center">Map Chart</h3>
               <ReactFC {...chartConfigs_map} />
+            </div> */}
+
+            {/*Map section*/}
+            <div className="col-sm-12" style={{marginTop:50}}>
+              <h3 className="text-center">Map Chart</h3>
+              <ReactFC {...chartConfigs_asiaMap} />
             </div>
           </div>
         </div>)
