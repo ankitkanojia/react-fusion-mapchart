1f36de6042022e74cdfbe92ef1661474a83f0195
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-10-04 21:40:20 +0530

    Integrate Map with country and world

--- src/App.js
+++ src/App.js
@@ -3,88 +3,84 @@ import FusionCharts from 'fusioncharts';
 import Maps from 'fusioncharts/fusioncharts.maps';
 
 //download all map from https://cdn.fusioncharts.com/downloads/addons/fusionmaps-xt-definition.zip
-// import WorldWithCountries from 'fusioncharts/maps/fusioncharts.worldwithcountries';
-import ReactFC from 'react-fusioncharts';
+import WorldWithCountries from 'fusioncharts/maps/fusioncharts.worldwithcountries';
+import ReactFCWorldWithCountries  from 'react-fusioncharts';
+import ReactFCWorld from 'react-fusioncharts';
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-
-// import Fusion Charts library
 import World from 'fusioncharts/maps/fusioncharts.world';
 
-ReactFC.fcRoot(FusionCharts, Maps, World, FusionTheme);
-
-// column 3d chart configuration settings
-const chartConfigs_map = {
-  type: 'world',
-  width: '100%',
-  height: 450,
-  dataFormat: 'json',
-  dataSource: {
-    "chart": {
-      "theme": "fusion",
-      "caption": "Increased Population History by country Last Year",
-      "xAxisName": "Population",
-      "yAxisName": "Growth",
-      "entityFillHoverColor": "#FFF9C4",
-      "numberSuffix": "%",
-      "showBorder": "1",
-      "borderColor": "#ccc",
-      "bgColor": "#FFFFFF",
-      //Tooltip customization
-      "toolTipBorderColor": "#666666",
-      "toolTipBgColor": "#efefef",
-      "toolTipBgAlpha": "80",
-      "showToolTipShadow": "1",
-      //Theme
-      "plotBorderThickness": "0.25",
-      "showxaxispercentvalues": "1",
-      "showValues": "1"
-    },
-    "colorrange": {
-      "color": [{
-        "minvalue": "0",
-        "maxvalue": "100",
-        "color": "#FFD74D"
-      }, {
-        "minvalue": "0",
-        "maxvalue": "100",
-        "color": "#FB8C00"
-      }, {
-        "minvalue": "0",
-        "maxvalue": "100",
-        "color": "#E65100"
-      }]
-    },
-    "data": [{
-      "id": "NA",
-      "value": "20",
-      "showLabel": "1"
-    }, {
-      "id": "SA",
-      "value": "30",
-      "showLabel": "1"
-    }, {
-      "id": "AS",
-      "value": "48",
-      "showLabel": "1"
-    }, {
-      "id": "EU",
-      "value": "20",
-      "showLabel": "1"
-    }, {
-      "id": "AF",
-      "value": "2.58",
-      "showLabel": "1"
-    }, {
-      "id": "AU",
-      "value": "15",
-      "showLabel": "1"
-    }]
-  },
-};
+ReactFCWorldWithCountries.fcRoot(FusionCharts, Maps, WorldWithCountries, FusionTheme);
+ReactFCWorld.fcRoot(FusionCharts, Maps, World, FusionTheme);
 
 class MapChart extends Component {
     state = {
-        dataSource: {
+        dataSource_world: {
+            "chart": {
+                canvasBorderColor: "#000000",
+                canvasBorderThickness: 1,
+                showBorder: true,
+                borderColor: "#000000",
+                showCanvasBorder: true,
+                theme: "fusion",
+                caption: "Increased Population History by country Last Year",
+                xAxisName: "Population",
+                yAxisName: "Growth",
+                entityFillHoverColor: "#FFF9C4",
+                numberSuffix: "%",
+                showBorder: "1",
+                bgColor: "#FFFFFF",
+                //Tooltip customization
+                toolTipBorderColor: "#666666",
+                toolTipBgColor: "#efefef",
+                toolTipBgAlpha: "80",
+                showToolTipShadow: "1",
+                //Theme
+                plotBorderThickness: "0.25",
+                showxaxispercentvalues: "1",
+                showValues: "1"
+            },
+            colorrange: {
+                "color": [{
+                    "minvalue": "0",
+                    "maxvalue": "100",
+                    "color": "#FFD74D"
+                }, {
+                    "minvalue": "0",
+                    "maxvalue": "100",
+                    "color": "#FB8C00"
+                }, {
+                    "minvalue": "0",
+                    "maxvalue": "100",
+                    "color": "#E65100"
+                }]
+            },
+            data: [{
+                "id": "NA",
+                "value": "20",
+                "showLabel": "1"
+            }, {
+                "id": "SA",
+                "value": "30",
+                "showLabel": "1"
+            }, {
+                "id": "AS",
+                "value": "48",
+                "showLabel": "1"
+            }, {
+                "id": "EU",
+                "value": "20",
+                "showLabel": "1"
+            }, {
+                "id": "AF",
+                "value": "2.58",
+                "showLabel": "1"
+            }, {
+                "id": "AU",
+                "value": "15",
+                "showLabel": "1"
+            }]
+        },
+        dataSource_worldWithCountry: {
             "chart": {
                 showCanvasBorder: true,
                 canvasBorderColor: "#000000",
@@ -146,21 +142,27 @@ class MapChart extends Component {
     render() {
         return (
             <React.Fragment>
-                <h2 style={{textAlign:"center"}}>Map Chart</h2>
-                {/* <div style={{ textAlign: 'center' }}>
-                    <ReactFC
+                <h2 style={{textAlign:"center"}}>Map With World Chart</h2>
+                <div style={{ textAlign: 'center' }}>
+                    <ReactFCWorldWithCountries
                         type="worldwithcountries"
                         width="80%"
                         height="500"
                         dataFormat="JSON"
-                        dataSource={this.state.dataSource} />
-                </div> */}
-
-                {/*Map section*/}
-                <div className="col-sm-12">
-                    <h3 className="text-center">Map Chart</h3>
-                    <ReactFC {...chartConfigs_map} />
+                        dataSource={this.state.dataSource_worldWithCountry} />
                 </div>
+                <br />
+                <h2 style={{ textAlign: "center" }}>Map World Chart</h2>
+                <div style={{ textAlign: 'center' }}>
+                    <ReactFCWorld
+                        type="world"
+                        width="80%"
+                        height="500"
+                        dataFormat="JSON"
+                        dataSource={this.state.dataSource_world} />
+                </div>
+                <br />
+                <br />
             </React.Fragment>);
     }
 }
